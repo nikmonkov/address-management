76a197477fcdbe072bd3e1c2987c756be9f65ddf
Nikolay Monkov <nikmonkov@users.noreply.github.com>
2022-04-17 07:36:04 +0300

    added some tests

--- src/test/java/com/nikmonkov/address/resource/AddressComponentResourceTest.java
+++ src/test/java/com/nikmonkov/address/resource/AddressComponentResourceTest.java
@@ -3,6 +3,7 @@ package com.nikmonkov.address.resource;
 import com.nikmonkov.address.database.entity.AddressComponentEntity;
 import com.nikmonkov.address.database.repo.AddressComponentRepository;
 import com.nikmonkov.address.model.AddressComponent;
+import com.nikmonkov.address.service.AddressService;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
 import io.restassured.http.ContentType;
@@ -11,9 +12,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.List;
 import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
 class AddressComponentResourceTest {
@@ -21,10 +24,16 @@ class AddressComponentResourceTest {
     @InjectMock
     AddressComponentRepository addressComponentRepository;
 
+    @InjectMock
+    AddressService addressService;
+
     @BeforeEach
     void setUp() {
         Mockito.when(addressComponentRepository.findById(Mockito.any()))
                 .thenReturn(new AddressComponentEntity("1", "test", null));
+
+        Mockito.when(addressService.searchAddress(Mockito.any()))
+                .thenReturn(List.of(new AddressComponent("1", "test", null)));
     }
 
     @Test
@@ -55,6 +64,10 @@ class AddressComponentResourceTest {
 
     @Test
     void searchByName() {
+        given()
+                .when().get("/api/v1/address/search?name=test")
+                .then().statusCode(200)
+                .body("size()", is(1));
     }
 
     @Test
