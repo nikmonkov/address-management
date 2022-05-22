80af70988875930ce4b6edfbc72597bbfe5593d9
Nikolay Monkov <nikmonkov@users.noreply.github.com>
2022-05-22 15:20:23 +0300

    checkstyle

--- src/main/java/com/nikmonkov/address/database/entity/AddressComponentEntity.java
+++ src/main/java/com/nikmonkov/address/database/entity/AddressComponentEntity.java
@@ -5,7 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Data
 @AllArgsConstructor
--- src/main/java/com/nikmonkov/address/resource/AddressComponentResource.java
+++ src/main/java/com/nikmonkov/address/resource/AddressComponentResource.java
@@ -7,7 +7,14 @@ import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.Collections;
 import java.util.List;
